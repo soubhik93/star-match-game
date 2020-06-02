e622e79cee00fdf89fd972374df35caf98de9937
sOubHik <soubhik93@gmail.com>
2020-06-02 23:56:45 +0530

    Updates

---
+++ precache-manifest.df035de190fb4bbb34d4d005f22adbf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc8dadfc61661a93babfc08f4a4976f",
+    "url": "/star-match-game/index.html"
+  },
+  {
+    "revision": "99a3dbd33df8dd01bc50",
+    "url": "/star-match-game/static/css/main.28259b14.chunk.css"
+  },
+  {
+    "revision": "5b130630405c7e842be4",
+    "url": "/star-match-game/static/js/2.8c2e3f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match-game/static/js/2.8c2e3f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a3dbd33df8dd01bc50",
+    "url": "/star-match-game/static/js/main.0da35f30.chunk.js"
+  },
+  {
+    "revision": "d211adc0bb4bbcdae1d8",
+    "url": "/star-match-game/static/js/runtime-main.c2ed47dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-match-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
